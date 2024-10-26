b9aa475b244d55530bfcb21683dc1f6741385d10
Pankaj Sharma <grgnld3970@gmail.com>
2024-10-26 17:27:05 +0530

    Push changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import axios from "axios";
+import React from "react";
 
-function App() {
-	const [jokes, setJokes] = useState([]);
-	useEffect(() => {
-		axios
-			.get("/jokes")
-			.then((response) => {
-				setJokes(response.data);
-				console.log(respnse.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	});
-	return (
-		<>
-			<h1>HI BERLIN</h1>
-			<h4>`Jokes={jokes.length}`</h4>
-			{jokes.map((joke, index) => (
-				<div key={joke.id}>
-					<h5>{joke.desc}</h5>
-					<h5>{joke.content}</h5>
-				</div>
-			))}
-		</>
-	);
-}
+const App = () => {
+	return <div>Hi github</div>;
+};
 
 export default App;
